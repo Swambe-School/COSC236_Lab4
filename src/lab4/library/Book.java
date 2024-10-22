77f962c749d1981165324a425c4096a69ee0d901
Brayden <coghill30@gmail.com>
2024-10-22 08:43:20 -0700

    Refactor Book class to improve code readability and maintainability. Added methods and attributes.

--- src/lab4/library/Book.java
+++ src/lab4/library/Book.java
@@ -1,14 +1,14 @@
 package lab4.library;
- 
+
 public class Book {
 
 	// GRASP Principle: Information Expert
 	// Manages its own state (available or borrowed).
 	private String title;
 	private String author;
-	private boolean isAvailable; 
+	private boolean isAvailable;
 	
-	// DONE: implement Book functionality 
+	// DONE: implement Book functionality
 	public Book(String title, String author){
 		this.title = title;
 		this.author = author;
