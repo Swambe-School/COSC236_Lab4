a49f5b21909dbf3984daf949dd8e6487d283d941
Brayden <coghill30@gmail.com>
2024-10-22 08:41:46 -0700

    Update .project file to include filtered resources

--- src/lab4/library/Book.java
+++ src/lab4/library/Book.java
@@ -5,8 +5,37 @@ public class Book {
 	// GRASP Principle: Information Expert
 	// Manages its own state (available or borrowed).
 	private String title;
+	private String author;
 	private boolean isAvailable; 
 	
-	// TODO: implement Book functionality 
-	
+	// DONE: implement Book functionality 
+	public Book(String title, String author){
+		this.title = title;
+		this.author = author;
+		this.isAvailable = true;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+
+	public String getAuthor(){
+		return author;
+	}
+
+	public String getInfo(){
+		return "Title: " + getTitle() + " Written by: " + getAuthor();
+	}
+
+	public boolean isAvailable(){
+		return isAvailable;
+	}
+
+	public void returnBook(){
+		this.isAvailable = true;
+	}
+
+	public void borrowBook(){
+		this.isAvailable = false;
+	}
 }
